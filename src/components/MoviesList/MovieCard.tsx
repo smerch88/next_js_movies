305c240ed1437a0d1665c90570687b00f72579b6
smerch88 <maxarmyk@gmail.com>
2023-03-10 16:36:30 +0200

    update: card styles

--- src/components/MoviesList/MovieCard.tsx
+++ src/components/MoviesList/MovieCard.tsx
@@ -7,10 +7,13 @@ import { useDispatch } from "react-redux";
 import { AppDispatch } from "@/redux/store";
 import { fetchGetMovieDetails } from "@/redux/movies/movies-operations";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 const MovieCard = ({ movie }: { movie: Movie }) => {
   const dispatch = useDispatch<AppDispatch>();
   const router = useRouter();
+
+  const [isCardClicked, setIsCardClicked] = useState(true);
 
   const { Title, Poster, Type, Year, imdbID } = movie;
 
@@ -19,19 +22,42 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
     router.push(`/movies/details?imdbID=${imdbID}&Title=${Title}`);
   };
 
+  const handleCardClick = () => {
+    setIsCardClicked(!isCardClicked);
+  };
+
+  const typographyStyle = {
+    textOverflow: isCardClicked ? "ellipsis" : "unset",
+    whiteSpace: isCardClicked ? "nowrap" : "unset",
+    overflow: isCardClicked ? "hidden" : "unset",
+  };
+
   return (
     <>
       <Card sx={{ minWidth: 275 }}>
-        <CardActionArea>
-          <CardMedia component="img" height="140" image={Poster} alt="poster" />
+        <CardActionArea onClick={handleCardClick}>
+          <CardMedia component="img" height="500" image={Poster} alt="poster" />
           <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              sx={typographyStyle}
+            >
               Title: {Title}
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={typographyStyle}
+            >
               Type: {Type}
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={typographyStyle}
+            >
               Year: {Year}
             </Typography>
           </CardContent>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,47 @@ import Image from "next/image";
 import cinemaImg from "../../public/cinema.jpg";
 import Head from "next/head";
 
-const Home = () => (
-  <>
-    <Head>
-      <title>Home</title>
-    </Head>
-    <h1>Movies search Home Page</h1>
-    <Image
-      src={cinemaImg}
-      width={300}
-      height={200}
-      alt="cinema"
-      placeholder="blur"
-    />
-  </>
-);
+import { Grid, Typography, Container } from "@mui/material";
+import MovieCard from "@/components/MoviesList/MovieCard";
+
+export async function getServerSideProps() {
+  const res =
+    await fetch(` https://www.omdbapi.com/?s=shrek&apikey=19759c28&page=1&type=movie
+`);
+  const data = await res.json();
+  return { props: { data } };
+}
+
+const Home = ({ data }: { data: MoviesSearchResult }) => {
+  const { Search } = data;
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <Container>
+        <Typography variant="h2" component="h1" mb={2}>
+          Best Movies of all times
+        </Typography>
+        <Grid container spacing={2}>
+          {Search &&
+            Search.slice(0, 3).map((movie: Movie) => (
+              <Grid item key={movie.imdbID} xs={12} sm={6} md={4}>
+                <MovieCard movie={movie} />
+              </Grid>
+            ))}
+        </Grid>
+        <Image
+          src={cinemaImg}
+          width={300}
+          height={200}
+          alt="cinema"
+          placeholder="blur"
+        />
+      </Container>
+    </>
+  );
+};
 
 export default Home;
